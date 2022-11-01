c07e5260c5c06092520090612cce4525eb3b7566
Timur Khamzin <timur.hamzin@gmmail.com>
2022-11-01 12:47:38 +0600

    Add encoding to with open

--- anfisa_for_friends/test.py
+++ anfisa_for_friends/test.py
@@ -61,7 +61,7 @@ def read_text_file_asserting_content(
             cleared_ = '\n'.join(map(str.strip, cleared_.split('\n')))
         return cleared_
 
-    with open(file_path, 'r') as fh:
+    with open(file_path, 'r', encoding='utf8') as fh:
         current_content = fh.read()
         current_content_cleared = clear_content(current_content)
         if expect_content:
